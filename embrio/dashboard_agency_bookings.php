27929183a7787d444a1e56440585bb9a6cca0517
jalurkerja <phantom@jalurkerja.com>
2018-01-11 14:34:29 +0700

    new update

--- embrio/dashboard_agency_bookings.php
+++ embrio/dashboard_agency_bookings.php
@@ -1,6 +1,5 @@
 <br>
 <div>
-	<table>
 		<?php 
 			$bookings = $db->fetch_all_data("bookings",[],"user_id='".$__user_id."'");
 			if(count($bookings) <= 0){
@@ -8,19 +7,18 @@
 			} else {
 				foreach($bookings as $booking){
 					$model = $db->fetch_all_data("model_profiles",[],"user_id='".$booking["book_user_id"]."'")[0];
-					$model_file = $db->fetch_all_data("model_files",[],"user_id='".$booking["book_user_id"]."'")[0];
+					$model_profile = $db->fetch_all_data("model_profiles",[],"user_id='".$booking["book_user_id"]."'")[0];
 			?>
-			<tr>
-				<td>									
-					<div class="col-sm-4">
-						<img style="margin-top:10px" src="user_images/<?=$model_file["filename"];?>" width="100">
+				<div class="col-sm-5" style="cursor:pointer;" onclick="window.open('model_details.php?user_id=<?=$booking["book_user_id"];?>');">
+					<div class="col-sm-12">
+						<img style="margin-top:10px" src="user_images/<?=$model_profile["photo"];?>" class="img-responsive">
 					</div>
-					<div class="col-sm-8">
+					<div class="col-sm-12">
 						<div><h3><?=$model["first_name"];?> <?=$model["middle_name"];?> <?=$model["last_name"];?> </h3></div>
 						<div><?=format_tanggal($booking["casting_start"]);?> s/d <?=format_tanggal($booking["casting_end"]);?></div>
 						<div><?=str_replace(chr(13).chr(10),"<br>",$booking["description"]);?></div>
 						<div><b>Rp. <?=format_amount($booking["fee"],2);?></b></div>
-						<div>Your token: <?=$booking["book_user_token"];?><div>
+						<div>Your token: <?=$booking["book_user_token"];?></div>
 						
 						<?php if($booking["status"] == "2"){ 
 							?> 
@@ -39,10 +37,9 @@
 						}?>
 					</div>
 					<div class="col-sm-12" style="padding-bottom:10px;border-bottom:1px solid #aaa;width:100%;"></div>
-				</td>
-			</tr>
+				</div>
+				<div class="col-sm-1"></div>
 			<?php } ?>
 		<?php } ?>
-	</table>
 </div>
 <br><br>
